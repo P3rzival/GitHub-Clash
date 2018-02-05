9256dff0b94cdf6bb63312174b4c5d8bd106367f
p3rzival <p3rzival@gmail.com>
2018-02-05 02:53:26 +0000

    final fully functional webapp

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var api = require('../utils/api');
+var Loading = require('./Loading');
 
 function SelectLanguage (props){
     var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -94,7 +95,7 @@ class Popular extends React.Component {
           selectedLanguage={this.state.selectedLanguage}
           onSelect={this.updateLanguage} />
         {!this.state.repos 
-        ? <p> Waiting for GitHub to send the info</p>
+        ? <Loading />
         : <RepoGrid repos={this.state.repos} />}
       </div>
     )
